a630c585d494b9e1d95591126dbaffbcd35d5747
ariel041596 <chocokises17@gmail.com>
2020-02-19 19:04:21 +0800

    Added Terms and Conditions on Checkout Page

--- src/components/Checkout/Checkout.vue
+++ src/components/Checkout/Checkout.vue
@@ -2,6 +2,9 @@
   <q-page>
     <template v-if="cart.length">
       <div class="text-subtitle2">
+        <q-card-section>
+          <div class="text-h6 text-primary">Formal Quotation</div>
+        </q-card-section>
         <q-list>
           <q-item v-for="item in cart" :key="item.id">
             <q-item-section top avatar>
@@ -40,6 +43,15 @@
           </q-item-label>
 
           <q-item-label class="text-grey-8 q-pb-xs" v-if="userDetails.roles == 'new'">
+            ADD 12% VAT
+            <strong>
+              {{
+              computeTax
+              | currency("₱", 2, { decimalSeparator: "." })
+              }}
+            </strong>
+          </q-item-label>
+          <q-item-label class="text-grey-8 q-pb-xs" v-if="userDetails.roles == 'new'">
             Mobilization Fee:
             <strong>
               {{
@@ -76,13 +88,60 @@
           </q-item-label>
         </div>
       </q-card-actions>
-      <div class="q-mt-xl">
+      <q-card-actions class>
+        <q-item-label class="q-ml-md">
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>1.</strong>
+            If there's a Lifting Equipment, test weight for load testing, as well as necessary equipment for moving the test weights will be provided by the
+            <strong>{{this.userDetails.companyName}}</strong>
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>2.</strong>
+            <strong>{{this.userDetails.companyName}}</strong> will provide a counterpart Safety Officer who will be the dedicated Liason Officer to coordinate with the other concerned parties within its organization.
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>3.</strong>
+            <strong>All equipment inspected, even if it is deemed that inspection is no longer necessary on that equipment, whether it passes or fails, will be included in the billing</strong>
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>4.</strong>
+            <strong>Grace Period:</strong> If equipment fails an inspection, there will be a
+            <strong>30-days grace period</strong>- within this period only mobilization will be billed for the reinspection. Beyond that period, it will be billed as new equipment.
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>5.</strong>
+            <strong>Equipment Breakdown.</strong>
+            PEOPLE360 is not responsible for any loss, destruction, or damage that may occur during the conduct of inspection of the units of equipment, nor any parts of said number of equipment, covered by this agreement.
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-xs">
+            <strong>6.</strong>
+            If DOLE Witness is required,
+            <strong>({{this.userDetails.companyName}})</strong> will make the necessary arrangements.
+          </q-item-label>
+          <q-item-label class="col text-justify q-pb-md">
+            <strong>7.</strong>
+            This qoutation is only valid for 6 months after the date of issuance.
+          </q-item-label>
+        </q-item-label>
+        <q-item-label class="col-6 text-justify q-pb-lg">Yours Very Truly,</q-item-label>
+        <div class="row">
+          <q-item-label class="col text-justify q-pb-lg">Conforme:</q-item-label>
+          <q-item-label class="col-12 text-justify q-pt-md">
+            For
+            <strong>{{this.userDetails.companyName}}</strong>
+          </q-item-label>
+        </div>
+        <div class="row col-6">
+          <q-item-label class="col-12 text-justify">(Safety Associate Name):</q-item-label>
+          <q-item-label class="col-12 text-justify">Safety Associate</q-item-label>
+        </div>
+        <div class="row col-6 q-mt-lg">
+          <q-item-label class="col text-justify q-pb-lg">(Signature Over Printed Name, Date)</q-item-label>
+        </div>
+      </q-card-actions>
+      <div class="q-mt-md">
         <q-form @submit.prevent="requestProposal">
           <q-card flat class="my-card bg-white text-primary">
-            <q-card-section>
-              <div class="text-h6">Pre Proposal Checklist</div>
-              <!-- <div class="text-subtitle2">by John Doe</div> -->
-            </q-card-section>
             <!-- <FullCalendar
               :plugins="calendarPlugins"
               :header="{
@@ -558,9 +617,14 @@ export default {
     //     !this.form.issued_by
     //   );
     // }
+    computeTax() {
+      return (this.totalPrice * 12) / 100;
+    },
     grandTotalforNew() {
       return (this.grandTotalforNewClient =
-        +this.totalPrice + +this.preproposal.mobilizationFee.newClientPrice);
+        +this.totalPrice +
+        +this.preproposal.mobilizationFee.newClientPrice +
+        (+this.totalPrice * 12) / 100);
     },
     grandTotalforOld() {
       return (this.grandTotalforOldClient =
